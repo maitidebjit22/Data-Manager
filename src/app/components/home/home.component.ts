12494462b5478e5a074bb5344f6067f3fae4eb17
Debjit Maiti <103867507+maitidebjit22@users.noreply.github.com>
2025-03-09 10:04:28 +0530

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,18 +2,23 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { DocumentService } from 'src/app/document.service';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { Chart, registerables } from 'chart.js'
+import { HttpClient } from '@angular/common/http';
+Chart.register(...registerables);
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  data:any;
   files: any[] = [];
+  dataname: any[] = [];
+  datasize: any[] = [];
   totalFiles: number = 0;
   totalUsers: number = 0;
   uploadCount: number = 0;
-  constructor( private _auth: AuthService, private router:Router,private fileService:DocumentService) { }
+  constructor( private _auth: AuthService, private router:Router,private fileService:DocumentService,private http: HttpClient) { }
 
   ngOnInit(): void {
   
@@ -27,6 +32,22 @@ export class HomeComponent {
     this.getUploadCount();
   }
   {
+    this.http.get<any[]>('http://localhost:4000/file//files').subscribe(res=>{
+      this.data=res;
+      if(this.data!=null)
+      {
+        for(let i=0;i<this.data.length;i++)
+        {
+          this.dataname.push(this.data[i].originalfile);
+          this.datasize.push(this.data[i].fileSize);
+          
+        }
+  
+      }
+      this.showchatdata(this.dataname,this.datasize);
+    })
+  }
+  {
     this.fileService.getFilesForToday().subscribe(
       (files) => {
         this.files = files;
@@ -37,6 +58,25 @@ export class HomeComponent {
     );
   }
 
+}
+showchatdata(dataname:any, datasize:any){
+  const myChart=new Chart("myChart", {
+    type: 'bar',
+    data: {
+      labels: dataname,
+      datasets: [{
+        label: '# of Size',
+        data: datasize,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      
+        indexAxis: 'y',
+        
+      
+    }
+  });
 }
   loadFileCounts(): void {
     this.fileService.getFileCounts().subscribe(data => {
